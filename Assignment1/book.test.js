df938a43d464a9333dfdf01b081a84d75269f879
sarahp27 <sarah.patel@xloopdigital.com>
2023-04-10 00:23:35 -0700

    Second Test Case Failed

--- Assignment1/book.test.js
+++ Assignment1/book.test.js
@@ -13,3 +13,19 @@ const {
     //Assert
     expect(get).toEqual(assert);
   });
+
+
+  test("TestNumberRead",())=>{
+    //Arrange
+    const Book = {
+        title: "The Hobbite", author: "Mirza Ghalib", length: 250, year: 2000 
+    };
+    const ReadDate = "20-10-2020" ;
+    const Rating = 4;
+    addBook(Book, ReadDate, Rating);
+    assert = 1;
+    //Act
+    let get = numberRead[];
+    //Assert
+    expect(get).toBe(assert);
+  }
